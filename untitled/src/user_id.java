d50a60929a9ee6203d15e79d0d39df4425d74bd9
almognaf <alnaftaly@gmail.com>
2020-05-11 19:32:22 +0300

    new update by almog

---
+++ untitled/src/user_id.java
@@ -0,0 +1,14 @@
+public class user_id
+{
+    private static Integer sum_id=0;//סטאטיק שיעזור לנו לעשות id
+    private Integer id=0;// המשתנה שנשים באמת בid של כל אובייקט
+
+    public user_id()
+    {
+        this.sum_id++;
+        this.id+=sum_id;
+    }
+    public Integer getId() {
+        return id;
+    }
+}
