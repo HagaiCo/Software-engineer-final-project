261110c7be7f33c30d627e05217c78e7be84c017
HagaiCo <50545614+hagai1994@users.noreply.github.com>
2020-05-17 12:49:12 +0300

    Adding DB tool for forward implementation.

--- untitled/src/MVCcalculator.java
+++ untitled/src/MVCcalculator.java
@@ -5,31 +5,12 @@ public class MVCcalculator {
 
     public static void main(String[] args)
     {
-        new MVCcalculator().DBTest();
+        new DBCreation().DBCreate();
         CalculationView theView = new CalculationView();
         CalculationModel theModel = new CalculationModel();
 
         CalculatorController controller = new CalculatorController(theModel, theView);
 
         theView.setVisible(true);
-    }
-    void DBTest()
-    {
-        String user = "postgres";
-        String pass = "Yeswecan2015";
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        try
-        {
-            Connection con = DriverManager.getConnection(url, user, pass);
-            Statement stm = con.createStatement();
-            String sql = "Select * from project";
-            ResultSet resStatement = stm.executeQuery(sql);
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-
-
     }
 }
