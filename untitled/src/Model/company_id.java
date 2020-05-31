e28459d3045ab31d0c53a743bf0cd16deb184b5f
almognaf <alnaftaly@gmail.com>
2020-05-31 12:50:10 +0300

    Advance GUI design by almog + DB

--- untitled/src/Model/company_id.java
+++ untitled/src/Model/company_id.java
@@ -1,14 +1,17 @@
-public class user_id
+package Model;
+
+public class company_id
 {
     private static Integer sum_id=0;//סטאטיק שיעזור לנו לעשות id
     private Integer id=0;// המשתנה שנשים באמת בid של כל אובייקט
 
-    public user_id()
-    {
+    public company_id() {
         this.sum_id++;
         this.id+=sum_id;
     }
+
     public Integer getId() {
         return id;
     }
+
 }
